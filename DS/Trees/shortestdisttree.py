24d2da5f1c1b57adef6a50ae7bb4869cfe80bcff
Sarvesh Dubey <38752758+dubesar@users.noreply.github.com>
2019-04-19 16:40:45 +0530

    Create shortestdisttree.py

---
+++ DS/Trees/shortestdisttree.py
@@ -0,0 +1,23 @@
+t = int(input())
+for _ in range(t):
+    i,j = map(int,input().split())
+    l = 0
+    while(True):
+        if(i==j):
+            break
+        else:
+            if(i>j):
+                if(i%2==0):
+                    i//=2
+                    l+=1
+                else:
+                    i = (i-1)//2
+                    l+=1
+            else:
+                if(j%2==0):
+                    j//=2
+                    l+=1
+                else:
+                    j = (j-1)//2
+                    l+=1
+    print(l)
