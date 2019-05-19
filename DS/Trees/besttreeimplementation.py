c80886c43d685e7d4d563aaadc126b027a093a08
Sarvesh Dubey <38752758+dubesar@users.noreply.github.com>
2019-05-19 08:33:34 +0530

    Update besttreeimplementation.py

--- DS/Trees/besttreeimplementation.py
+++ DS/Trees/besttreeimplementation.py
@@ -1,3 +1,5 @@
+import sys
+sys.setrecursionlimit(1500)
 class Node:
     def __init__(self,key):
         self.val = key
@@ -18,8 +20,33 @@ def insert(root,node):
                 root.left = node
             else:
                 insert(root.left,node)
+def find(root, x):
+    if root.val == x:
+        pre(r)
+    elif root.val > x:
+        find(root.left, x)
+    elif root.val < x:
+        find(root.right, x)
 def pre(root):
     if root:
         print(root.val)
         pre(root.left)
         pre(root.right)
+def height(root):
+    if(root is None):
+        return 0
+    else:
+        ldepth = height(root.left)
+        rdepth = height(root.right)
+
+        if(ldepth>rdepth):
+            return ldepth+1
+        else:
+            return rdepth+1
+n = int(input())
+a = list(map(int,input().split()))
+q = int(input())
+r =  Node(a[0])
+for i in range(1,len(a)):
+    insert(r,Node(a[i]))
+find(r,q)
